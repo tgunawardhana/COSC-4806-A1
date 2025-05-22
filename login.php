8fb9b20d458c21f709174b50f81b88612e6b1254
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-22 17:51:49 +0000

    Updated login page with a form - Not Functional

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+  <body>
+
+    <h1>Login</h1>
+
+    <form action="/validate.php" method="post">
+      <label for="fname">Username:</label>
+      <br>
+      <input type="text" id="username" name="username">
+      <br>
+      <label for="password">Password:</label>
+      <br>
+      <input type="password" id="password" name="password">
+      <br><br>
+      <input type="submit" value="Submit">
+    </form>
+
+  </body>
+</html>
+
+
+
