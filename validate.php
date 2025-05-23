530e49be73573604ad3459e1fdbbe428ad59e05f
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-23 01:36:32 +0000

    Updated to go to login page after failed login

--- validate.php
+++ validate.php
@@ -3,7 +3,7 @@
 session_start();
 
 $valid_username = "teddy";
-$valid_password = "rules";
+$valid_password = "password";
 
 $username = $_REQUEST['username'];
 $_SESSION['username'] = $username;
@@ -11,6 +11,7 @@ $password = $_REQUEST['password'];
 
 if ($valid_username == $username && $valid_password == $password){
     $_SESSION['authenticated'] = 1;
+     $_SESSION['failed'] = 0;
     header("location: /");
 } else {
     if(!isset($_SESSION['failed_attempts'])){
@@ -18,8 +19,10 @@ if ($valid_username == $username && $valid_password == $password){
     } else {
       $_SESSION['failed_attempts'] += 1;
     }
-
-   echo "This is unsuccessful attempt number " . $_SESSION['failed_attempts'];
+  
+    $_SESSION['failed'] = 1;  
+    header("location: /login.php");
+    //echo "This is unsuccessful attempt number " . $_SESSION['failed_attempts'];
 
 }
 
